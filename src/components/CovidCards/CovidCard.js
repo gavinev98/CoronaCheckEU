5e18f4ed70cc2d51e60e949218a18b4532145dfe
Gavin Everett <gavin.everett48@hotmail.com>
2021-10-25 15:58:19 +0100

    working on getting global stats.

--- src/components/CovidCards/CovidCard.js
+++ src/components/CovidCards/CovidCard.js
@@ -9,7 +9,7 @@ import { green, pink } from '@mui/material/colors';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Grid';
-
+import { useState } from 'react';
 import useStyles from './CovidCardStyles.js';
 
 
@@ -19,21 +19,24 @@ const CovidCard = ({ globalStats }) => {
 
    const classes = useStyles();
 
+   //set state for just global statistics.
+   const[globalData, setGlobalData] = useState(globalStats.Global);
 
+ 
     return (
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-    <Grid item xs={6}>
-  {Object.keys(globalStats).map((keyName, keyValue) => (
+      <Grid container spacing={1}>
+     <Grid container item spacing={3}>
+  {Object.keys(globalData).map((keyName, keyValue) => (
   <Card key={keyValue} style={{ width: 200, height: 200}}>
       <CardContent>
         <Typography  className={classes.customTypographyH1} gutterBottom>
-        {globalStats.Global.keyName}
+        {keyName}
         </Typography>
         <Avatar sx={{ bgcolor: pink[500] }}>
           <PageviewIcon />
          </Avatar>
         <Typography sx={{ mb: 1.5, textAlign: 'center' }} color="text.secondary">
-     
+        
         </Typography>
         <Typography variant="body2" style={{textAlign: 'center'}}>
           Last Update : 14:30
