bede28d1a1be0816f3c885923bdde66babb9b1ce
Gavin Everett <gavin.everett48@hotmail.com>
2021-11-18 11:29:28 +0000

    updating api call.

--- src/api/covidData.js
+++ src/api/covidData.js
@@ -5,4 +5,4 @@ export const covidInfections = () => axios.get('https://api.covid19api.com/summa
 //export call for list of countries supported by api.
 export const covidCountries = () => axios.get('https://api.covid19api.com/countries');
 //api call to retrieve country selected  data.
-export const covidCountry  = (country, date) => axios.get(`https://api.covid19api.com/total/country/${country}status/confirmed/date/${date}`);
+export const covidCountry  = (country, date) => axios.get(`https://api.covid19api.com/live/country/${country}/status/confirmed/date/${date}`);
